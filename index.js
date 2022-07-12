a50d4d322a84aeb325f6b106b40a5a4e0024cd7b
Ismael Martínez <ismael_rafal@hotmail.com>
2022-07-12 22:27:18 +0200

    arreglos

--- index.js
+++ index.js
@@ -26,7 +26,6 @@ connection.connect();*/
 const express = require('express');
 const mysql = require('mysql');
 const cors = require('cors');
-
 const bodyParser = require('body-parser');
 
 const PORT = process.env.PORT || 3050;

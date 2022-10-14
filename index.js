e7c5a46c8b749399cac4db7fc72ea0e2b129fa10
Ismael Martínez <ismael_rafal@hotmail.com>
2022-10-14 09:58:18 +0200

    prueba para codesandbox

--- index.js
+++ index.js
@@ -40,7 +40,7 @@ const DB_USER = process.env.DB_USER || 'root';
 const DB_PASSWORD = process.env.DB_PASSWORD || '';
 const DB_DATA = process.env.DB_DATA || '';
 const DB_PORT = process.env.DB_PORT || 3050;
-const DB_NAME = process.env.DB_NAME || '';
+//const DB_NAME = process.env.DB_NAME || '';
 
 // MySql
 const connection = mysql.createConnection(

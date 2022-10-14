6a5f403d9c5a7ba2c48debd1b9d49c05a4fa3974
Ismael Martínez <ismael_rafal@hotmail.com>
2022-10-14 10:02:49 +0200

    prueba para codesandbox

--- index.js
+++ index.js
@@ -38,7 +38,7 @@ app.use(cors());
 const DB_HOST = process.env.DB_HOST || 'localhost';
 const DB_USER = process.env.DB_USER || 'root';
 const DB_PASSWORD = process.env.DB_PASSWORD || '';
-const DB_DATA = process.env.DB_DATA || '';
+const DB_DATA = process.env.DB_DATA || 'historia';
 const DB_PORT = process.env.DB_PORT || 3050;
 //const DB_NAME = process.env.DB_NAME || '';
 
